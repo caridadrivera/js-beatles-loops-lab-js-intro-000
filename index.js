cd512fd3494569b964d4db94a1d99128098809a4
lively-teacher-9822 <ccrive10@gmail.com>
2018-12-14 13:04:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var music =[];
+  for (var i = 0; i < musicians.length; i++) {
+    music.push(`${musicians[i]} plays ${instruments[i]}`);
+  }   
+  return music;
+}
